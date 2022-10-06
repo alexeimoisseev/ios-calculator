c24353a8b26857231a18b1154e069ca5f99272fd
Alexei Moisseev <alexmois@ya.ru>
2022-10-06 16:25:14 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Alexei Moisseev on 06.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
